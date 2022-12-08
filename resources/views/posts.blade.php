040d9c16f3b622a9ffd2b1e6ba0b939c90e945f6
Mehrdad <mehrdad.chatrazar1998@gmail.com>
2022-12-08 18:12:04 +0330

    Initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="app.css">
+    <title>My Blog</title>
+</head>
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My first post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam doloremque tempore, fuga exercitationem
+            consectetur suscipit ipsa ipsum quod temporibus distinctio architecto odit laboriosam ab. Facere odio blanditiis
+            rem nihil dolores.
+            Adipisci amet pariatur ea accusantium magni asperiores enim! Tempora minima quasi aperiam cupiditate repellat
+            amet doloribus enim, alias molestias earum iure odio eligendi. Minus nihil exercitationem, iusto vel inventore
+            voluptatum.
+            Consectetur distinctio rerum nobis, voluptatum tenetur ipsa debitis labore voluptate in enim atque! Soluta
+            similique repellendus provident ex! Consequatur consequuntur eaque saepe distinctio tempore totam sit in, beatae
+            nisi cumque.
+            Quia natus corporis nam error, quo exercitationem nulla ullam nemo eos inventore hic dolores expedita! Mollitia
+            nostrum earum, magnam esse illo optio numquam ex quam necessitatibus, quasi laboriosam ullam id.</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">My second post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam doloremque tempore, fuga exercitationem
+            consectetur suscipit ipsa ipsum quod temporibus distinctio architecto odit laboriosam ab. Facere odio blanditiis
+            rem nihil dolores.
+            Adipisci amet pariatur ea accusantium magni asperiores enim! Tempora minima quasi aperiam cupiditate repellat
+            amet doloribus enim, alias molestias earum iure odio eligendi. Minus nihil exercitationem, iusto vel inventore
+            voluptatum.
+            Consectetur distinctio rerum nobis, voluptatum tenetur ipsa debitis labore voluptate in enim atque! Soluta
+            similique repellendus provident ex! Consequatur consequuntur eaque saepe distinctio tempore totam sit in, beatae
+            nisi cumque.
+            Quia natus corporis nam error, quo exercitationem nulla ullam nemo eos inventore hic dolores expedita! Mollitia
+            nostrum earum, magnam esse illo optio numquam ex quam necessitatibus, quasi laboriosam ullam id.</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">My third post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam doloremque tempore, fuga exercitationem
+            consectetur suscipit ipsa ipsum quod temporibus distinctio architecto odit laboriosam ab. Facere odio blanditiis
+            rem nihil dolores.
+            Adipisci amet pariatur ea accusantium magni asperiores enim! Tempora minima quasi aperiam cupiditate repellat
+            amet doloribus enim, alias molestias earum iure odio eligendi. Minus nihil exercitationem, iusto vel inventore
+            voluptatum.
+            Consectetur distinctio rerum nobis, voluptatum tenetur ipsa debitis labore voluptate in enim atque! Soluta
+            similique repellendus provident ex! Consequatur consequuntur eaque saepe distinctio tempore totam sit in, beatae
+            nisi cumque.
+            Quia natus corporis nam error, quo exercitationem nulla ullam nemo eos inventore hic dolores expedita! Mollitia
+            nostrum earum, magnam esse illo optio numquam ex quam necessitatibus, quasi laboriosam ullam id.</p>
+    </article>
+</body>
+
+</html>
