e8593b0485b70ffdd4522cdaa1e94989bd735903
Mehrdad <mehrdad.chatrazar1998@gmail.com>
2022-12-09 00:03:57 +0330

    dynamic posts page

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\File;
+
 
 class Post
 {
+    public static function all(){
+        $files = File::files(resource_path("posts/"));
+
+        return array_map(function($file){
+            return $file->getContents();
+        }, $files);
+    }
+
+
     public static function find($slug)
     {
         if (!file_exists($path = resource_path("/posts/{$slug}.html"))) {
@@ -18,4 +29,5 @@ class Post
         });
 
     }
+
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,48 +10,11 @@
 </head>
 
 <body>
+    @foreach ($posts as $post)
     <article>
-        <h1><a href="/posts/my-first-post">My first post</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam doloremque tempore, fuga exercitationem
-            consectetur suscipit ipsa ipsum quod temporibus distinctio architecto odit laboriosam ab. Facere odio blanditiis
-            rem nihil dolores.
-            Adipisci amet pariatur ea accusantium magni asperiores enim! Tempora minima quasi aperiam cupiditate repellat
-            amet doloribus enim, alias molestias earum iure odio eligendi. Minus nihil exercitationem, iusto vel inventore
-            voluptatum.
-            Consectetur distinctio rerum nobis, voluptatum tenetur ipsa debitis labore voluptate in enim atque! Soluta
-            similique repellendus provident ex! Consequatur consequuntur eaque saepe distinctio tempore totam sit in, beatae
-            nisi cumque.
-            Quia natus corporis nam error, quo exercitationem nulla ullam nemo eos inventore hic dolores expedita! Mollitia
-            nostrum earum, magnam esse illo optio numquam ex quam necessitatibus, quasi laboriosam ullam id.</p>
+        <?= $post ?>
     </article>
-    <article>
-        <h1><a href="/posts/my-second-post">My second post</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam doloremque tempore, fuga exercitationem
-            consectetur suscipit ipsa ipsum quod temporibus distinctio architecto odit laboriosam ab. Facere odio blanditiis
-            rem nihil dolores.
-            Adipisci amet pariatur ea accusantium magni asperiores enim! Tempora minima quasi aperiam cupiditate repellat
-            amet doloribus enim, alias molestias earum iure odio eligendi. Minus nihil exercitationem, iusto vel inventore
-            voluptatum.
-            Consectetur distinctio rerum nobis, voluptatum tenetur ipsa debitis labore voluptate in enim atque! Soluta
-            similique repellendus provident ex! Consequatur consequuntur eaque saepe distinctio tempore totam sit in, beatae
-            nisi cumque.
-            Quia natus corporis nam error, quo exercitationem nulla ullam nemo eos inventore hic dolores expedita! Mollitia
-            nostrum earum, magnam esse illo optio numquam ex quam necessitatibus, quasi laboriosam ullam id.</p>
-    </article>
-    <article>
-        <h1><a href="/posts/my-third-post">My third post</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam doloremque tempore, fuga exercitationem
-            consectetur suscipit ipsa ipsum quod temporibus distinctio architecto odit laboriosam ab. Facere odio blanditiis
-            rem nihil dolores.
-            Adipisci amet pariatur ea accusantium magni asperiores enim! Tempora minima quasi aperiam cupiditate repellat
-            amet doloribus enim, alias molestias earum iure odio eligendi. Minus nihil exercitationem, iusto vel inventore
-            voluptatum.
-            Consectetur distinctio rerum nobis, voluptatum tenetur ipsa debitis labore voluptate in enim atque! Soluta
-            similique repellendus provident ex! Consequatur consequuntur eaque saepe distinctio tempore totam sit in, beatae
-            nisi cumque.
-            Quia natus corporis nam error, quo exercitationem nulla ullam nemo eos inventore hic dolores expedita! Mollitia
-            nostrum earum, magnam esse illo optio numquam ex quam necessitatibus, quasi laboriosam ullam id.</p>
-    </article>
+    @endforeach
 </body>
 
 </html>
